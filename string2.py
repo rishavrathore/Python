c38f0cf6c3523976a4cb661c735877fce7faba85
rishavrathore <114799874+rishavrathore@users.noreply.github.com>
2022-11-20 16:24:34 +0530

    Add files via upload

---
+++ string2.py
@@ -0,0 +1,13 @@
+a = "Hello Peoples"
+print(len(a))
+print(a[3])
+print(a[1:7])
+print(a[8:])
+print(a[:8])
+print(a[-3:])
+print(a[:-3])
+print(a[-7:-3])
+print(a[-3:-7:-1])
+
+print(a.upper())
+print(a.lower())
