fe277a51215d3e31d05a262a59df9f122c9843d7
rishavrathore <114799874+rishavrathore@users.noreply.github.com>
2022-11-21 22:32:56 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,46 @@
+'''a = 5 
+b = 10
+if b<a:
+    print("Yes")
+else:
+    print("NO")'''
+    # Take x and y two variables and assign them value
+    # If x is less than y, print YAYY
+    #else print NAYY
+
+'''x = int(input("Enter a number"))
+y = int(input("Enter another number"))
+if x>y:
+    print("YAYY")
+else:
+    print("NAYY")'''
+#take values of length and breadth from user
+#print if it is a square or not
+
+
+
+'''l = int(input("Enter length"))
+b = int(input("Enter breadth"))
+if l == b:
+    print("It is a square")
+else:
+    print("It is not a square")'''
+
+    # A Company decided to give bonus of 1000Rs to employee if his/her service 
+    # is more than 5 years. Ask user their salary and year of service and print 
+    # the net bonus amount.
+
+
+x = int(input("What is your salary ?"))
+y = int(input("What are your years of service"))
+if y>5:
+    print("Your net bonus amount is ",x+1000)
+else:
+    print("Sorry you are not eligible for bonus ,better luck next time")
+
+
+
+
+
+
+
