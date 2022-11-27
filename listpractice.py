49dbca44f782929c2e4f39b2d80b1d27278d40c3
Rishav Kumar Singh <114799874+rishavrathore@users.noreply.github.com>
2022-11-27 09:36:30 +0530

    Add files via upload

---
+++ listpractice.py
@@ -0,0 +1,82 @@
+# colors = ["Red" , "Blue" , "Black" ,22]
+# cars = ["TATA" , "NANO","ALTO", "Jeep"]
+# num = [1,2,34,5,2,6]
+# num2 = num.copy()
+# print(num2)
+# for i in range(len(num)):
+#     if num[i]==2:
+#         break
+# num.remove(num[i])
+# num.insert(i,200)
+# print(num)    
+#numbers = [1,2,3,4,5,6]
+# new_list =[]
+# for i in numbers:
+#     x = i**2
+#     new_list.append(x)
+# print(new_list)    
+#newList = [ x for x in cars if x != "TATA"]
+#print(newList)
+#[print(x) for x in cars]
+# newList = []
+# for i in cars:
+#     if "A" in i:
+#         newList.append(i)
+# print(newList)        
+#here we change colors name by taking range
+# colors[0:2] = ["Green","Yellow"]
+#we take insert to change a specific word
+# colors.insert(2,"indigo")
+#we take append to add another word
+# colors.append("BMW")
+#we take pop to remove a specific word
+# colors.pop(1)
+# print(colors)
+#we take remove to remove that word
+# colors.remove(22)
+# print(colors)
+#to clear all things
+# colors.clear()
+# print(colors)
+# for x in cars:
+#     print(x)
+# x = 0
+# while x < len(cars):
+#     print(cars[x])
+#     x+=1    
+list1 = ["x","y","z"]
+list2 = [1,2,3]
+
+list3 = list1 + list2
+print(list3)
+#list stores multiple data type or list are used storemmultiple non homogenous data in a single variable
+# ordered
+# changable
+#allows duplicate value
+
+# insert() insert new item at specified index
+# Append() insert item at the end of the list
+# Extend() it attends element from one list to another list, it can used to append different 
+# collection like adding element of list in tuple
+# remove() remove item from a specified index
+# pop() remove last element if index is not specified and if specified it remove specific element
+# clear() clear all elements 
+#range(), len()
+#sort() it will sort the list in ascending order
+#copy() it will create copy of the list
+
+s = [1,]
+#List are used store multiple non-homogeneous data in a single variable
+# ordered
+# Changable
+# Allows duplicate value
+# 
+# Insert() - insert item  at specified index
+# Append() - insert item at the end of the list
+# Extend() - it appends elements from one list to another list, it can be used to append different collection
+# Remove() - removes item from a specified index
+# pop()
+# clear() - clears all elements
+# range(), len()
+# sort() - it will sort the list in ascending order
+# copy() 
