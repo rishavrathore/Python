fdf0a14ed3a18a39b8ea0237a9b60928fdf53931
Rishav Kumar Singh <114799874+rishavrathore@users.noreply.github.com>
2022-11-27 09:36:51 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,36 @@
+# tuples stores multiple items in a single variable
+# ordered and are unchangable
+# use() for tuples 
+# and this allows duplicate values
+
+# myTuple = ("apple","pineapple","oranges")
+
+# tuple1 = ("q","r","s")
+# del(tuple1)
+# print(tuple1)
+
+# tuple1 = (1,2,3,4,5)
+# print(tuple1[::-1])
+
+# name = ("RISHAV",)
+# print(name)
+
+# tuple1 = (10,20,30,40)
+# (one, two, three,four) = tuple1
+# print(one)
+# print(two)
+# print(three)
+# print(four)
+
+# tuple1 = (10,20)
+# tuple2 = (30,40)
+
+# tuple1,tuple2=tuple2,tuple1
+# print(tuple1)
+# print(tuple2)
+
+tuple1 = (1,[6,7],2,3,4,5)
+tuple1 [1][0]=8
+print(tuple1)
+
+
